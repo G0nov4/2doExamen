94a1cacf7c42fb150f9b8b5c7cc6e0965ca584ea
G0nov4 <garynova00@gmail.com>
2023-06-12 11:51:00 -0400

    Ejercciio 3 y 5

--- P3/p3.py
+++ P3/p3.py
@@ -2,38 +2,9 @@ import pandas as pd
 import numpy as np
 import random
 
-# Leer el archivo CSV
-grafo = pd.read_csv("grafo_rutas.csv", index_col=0)
-print(grafo)
-# Convertir el CSV en una matriz numpy
-rutas = grafo.to_numpy()
 
-# Definir el número de nodos en el grafo
-n_nodos = len(rutas)
-
-# Definir el número de individuos en la población
-n_poblacion = 10
-
-# Definir la probabilidad de mutación
-mutacion = 0.3
-
-# Definir el número de generaciones
-n_generaciones = 200
-
-# Definir el nodo inicial
-nodo_inicial = 0
-
-# Función para calcular la distancia total de un recorrido
-def calcular_distancia_total(recorrido):
-    distancia_total = 0
-    for i in range(len(recorrido) - 1):
-        nodo_actual = recorrido[i]
-        nodo_siguiente = recorrido[i + 1]
-        distancia_total += rutas[nodo_actual][nodo_siguiente]
-    return distancia_total
-
-# Función para generar una población inicial aleatoria
-def generar_poblacion_inicial():
+#  Esta función genera una población inicial aleatoria de recorridos. Cada recorrido es una permutación aleatoria de los nodos del grafo.
+def generar_poblacion_inicial(): # GENERAR POBLACION INICIAL ALEATORIA
     poblacion = []
     for _ in range(n_poblacion):
         recorrido = list(range(n_nodos))
@@ -41,8 +12,8 @@ def generar_poblacion_inicial():
         poblacion.append(recorrido)
     return poblacion
 
-# Función para realizar el cruce de dos padres
-def cruzar_padres(padre1, padre2):
+#Realiza el cruce de dos padres seleccionados para generar un hijo. El cruce se realiza en un punto aleatorio y combina las partes de los padres para formar un nuevo recorrido.
+def crucePadresPoblacion(padre1, padre2): # CRUCE DE PADRES
     punto_cruce = random.randint(1, n_nodos - 1)
     hijo = padre1[:punto_cruce]
     for nodo in padre2:
@@ -50,52 +21,62 @@ def cruzar_padres(padre1, padre2):
             hijo.append(nodo)
     return hijo
 
-# Función para realizar la mutación en un individuo
-def mutar_individuo(individuo):
+# Aplica mutación a la población seleccionada. La mutación intercambia aleatoriamente la posición de dos nodos en el recorrido. 
+def mutacionSeleccionada(populationSelect): # MUTACION DEL SELECCINADO
     for _ in range(int(n_nodos * mutacion)):
-        idx1, idx2 = random.sample(range(1, n_nodos), 2)
-        individuo[idx1], individuo[idx2] = individuo[idx2], individuo[idx1]
-    return individuo
+        id1, id2 = random.sample(range(1, n_nodos), 2)
+        populationSelect[id1], populationSelect[id2] = populationSelect[id2], populationSelect[id1]
+    return populationSelect
 
-# Función para seleccionar los mejores individuos de la población
-def seleccionar_mejores(poblacion, fitness, n_seleccionados):
+#  Selecciona los mejores individuos de la población actual basados en su fitness (distancia total recorrida). Los mejores individuos se eligen mediante ordenamiento de los valores de fitness.
+def bestSelect(poblacion, fitness, n_seleccionados): # SELECCIONAMOS A LOS MEJORES INDIVIDUOS DE LA POBLACION
     mejores_indices = np.argsort(fitness)[:n_seleccionados]
     mejores_poblacion = [poblacion[idx] for idx in mejores_indices]
     return mejores_poblacion
 
-# Generar la población inicial
-poblacion = generar_poblacion_inicial()
 
-# Realizar las generaciones
-for generacion in range(n_generaciones):
-    # Calcular el fitness de cada individuo en la población
-    fitness = [calcular_distancia_total(recorrido) for recorrido in poblacion]
-    
-    # Seleccionar los mejores individuos
-    mejores_poblacion = seleccionar_mejores(poblacion, fitness, n_poblacion // 2)
-    
-    # Crear la nueva generación cruzando los mejores individuos
-    nueva_generacion = []
-    while len(nueva_generacion) < n_poblacion:
+def totalDistance(recorrido):
+    tDistance = 0
+    for i in range(len(recorrido) - 1):
+        nodo_actual = recorrido[i]
+        nodo_siguiente = recorrido[i + 1]
+        tDistance += rutas[nodo_actual][nodo_siguiente]
+    return tDistance
+
+
+
+
+
+
+# Leer el archivo CSV
+grafo = pd.read_csv("grafo_rutas.csv", index_col=0)
+print(grafo)
+rutas = grafo.to_numpy()
+n_nodos = 5 # RUTAS
+n_poblacion = 10 # POBLACION
+mutacion = 0.3 # PROBABILIDAD DE MUTACION
+n_generaciones = 200 # NUMERO DE GENERACIONES
+nodo_inicial = 0
+
+
+poblacion = generar_poblacion_inicial() # GNEREACION DE POBLACION INICIAL
+
+for generacion in range(n_generaciones): # ITERACION DE GENERACIONES 
+    fitness = [totalDistance(recorrido) for recorrido in poblacion] # CALCULAMOS LA MEDIDA DE CANTIDAD DE RECORRIDO (FITNESS)
+    mejores_poblacion = bestSelect(poblacion, fitness, n_poblacion // 2) # SELECCIONAMOS LOS MEJORES DE LA POBLACION
+    newGeneration = [] # CREAMOS UNA NUEVA GENERACION PARA LOS INDIVIDUOS
+    while len(newGeneration) < n_poblacion:
         padre1, padre2 = random.sample(mejores_poblacion, 2)
-        hijo = cruzar_padres(padre1, padre2)
-        nueva_generacion.append(hijo)
-    
-    # Aplicar la mutación a la nueva generación
-    nueva_generacion = [mutar_individuo(individuo) for individuo in nueva_generacion]
-    
-    # Reemplazar la población anterior con la nueva generación
-    poblacion = nueva_generacion
-
-# Seleccionar el mejor individuo de la última generación
-fitness = [calcular_distancia_total(recorrido) for recorrido in poblacion]
+        hijo = crucePadresPoblacion(padre1, padre2)
+        newGeneration.append(hijo)
+    newGeneration = [mutacionSeleccionada(individuo) for individuo in newGeneration] # APLICAMOS MUTACION EN LA NUEVA GENERACION
+    poblacion = newGeneration # REEMPLAZAMOS LA OLD GENERATION POR LA NUEVA GENERACION
+fitness = [totalDistance(recorrido) for recorrido in poblacion] # SELECCIONAMOS AL MEJOR INDIVIDO DE LA NUEVA GENERACION
 mejor_individuo_idx = np.argmin(fitness)
 mejor_recorrido = poblacion[mejor_individuo_idx]
-
-# Obtener el mejor recorrido iniciando en el nodo X
-indice_nodo_inicial = mejor_recorrido.index(nodo_inicial)
-mejor_recorrido = mejor_recorrido[indice_nodo_inicial:] + mejor_recorrido[:indice_nodo_inicial]
+indice_nodo_inicial = mejor_recorrido.index(nodo_inicial)                                           # OBTENEMOS EL MEJOR RECORRIDO
+mejor_recorrido = mejor_recorrido[indice_nodo_inicial:] + mejor_recorrido[:indice_nodo_inicial]     # DEL NODO
 
 # Imprimir el mejor recorrido y su distancia total
 print("Mejor recorrido:", mejor_recorrido)
-print("Distancia total:", calcular_distancia_total(mejor_recorrido))
+print("Distancia total:", totalDistance(mejor_recorrido))
